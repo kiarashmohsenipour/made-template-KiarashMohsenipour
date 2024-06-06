f9f7129135c09cb9b869ddc340ae18e4f06c8cf4
kiarashmohsenipour <kiarashmohsenipoor99@gmail.com>
2024-06-06 10:21:38 +0200

    Project 4 changes

---
+++ project/pipeline.py
@@ -0,0 +1,40 @@
+import io
+import os
+import sqlite3
+import pandas as pd
+import requests
+import matplotlib.pyplot as plt
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+try:
+    # Define paths
+    data_path = './data'
+    db_name = 'temperature_inflation.db'
+    # Kaggle dataset identifiers
+    URL_1 = "adamwurdits/finland-norway-and-sweden-weather-data-20152019"
+    URL_2 = "sazidthe1/global-inflation-data"
+    # Download datasets from Kaggle
+    api = KaggleApi()
+    api.authenticate()
+
+
+    api.dataset_download_files(URL_1, path=data_path, unzip=True)
+    api.dataset_download_files(URL_2, path=data_path, unzip=True)
+
+    csv_files = [f for f in os.listdir(data_path) if f.endswith('.csv')]
+
+    temperature_data = pd.read_csv(os.path.join(data_path, csv_files[0]))
+    inflation_data = pd.read_csv(os.path.join(data_path, csv_files[1]))
+
+    conn = sqlite3.connect('../data/temperature_inflation.db')
+
+    temperature_data.to_sql('temperature_data', conn, if_exists='replace', index=False)
+
+    # Insert data into the inflation_data table
+    inflation_data.to_sql('inflation_data', conn, if_exists='replace', index=False)
+
+    # Commit the changes and close the connection
+    conn.commit()
+    conn.close()
+except Exception as e:
+    print(f"error: {e}")
